0026d281c3bec4b04e45cc8fae6694ef46b5f060
Mohammed Ali Davami <69807363+moadav@users.noreply.github.com>
2022-01-30 07:03:08 +0100

    Opprettet api klasse, som håndterer å hente informasjon fra været

---
+++ DigitalTvilling/DigitalTvillingKlima/ApiInfo/Api.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace DigitalTvillingKlima
+{
+    public static class Api
+    {
+        public static HttpClient Client { get; set; } 
+
+
+        public static void initalizeApi()
+        {
+            Client = new HttpClient();
+            Client.DefaultRequestHeaders.Accept.Clear();
+            Client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+            Client.DefaultRequestHeaders.Add("User-Agent", "C# App, digital twins which collects weather data each hour");
+
+        }
+       
+
+    }
+}
