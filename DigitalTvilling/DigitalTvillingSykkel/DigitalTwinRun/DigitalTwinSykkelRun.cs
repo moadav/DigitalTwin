20cce650489c14bb22baa5274af6540499a4a0a2
Mohammed Ali Davami <69807363+moadav@users.noreply.github.com>
2022-03-08 18:29:32 +0100

    error håndtrering for sykkelapi

--- DigitalTvilling/DigitalTvillingSykkel/DigitalTwinRun/DigitalTwinSykkelRun.cs
+++ DigitalTvilling/DigitalTvillingSykkel/DigitalTwinRun/DigitalTwinSykkelRun.cs
@@ -10,32 +10,52 @@ namespace DigitalTvillingSykkel.DigitalTwinRun
 {
     public class DigitalTwinSykkelRun
     {
-        public async void RunSykkel()
+        public void RunSykkel()
         {
             ApiSykkel.InitalizeSykkelApi();
-            
-            using (HttpResponseMessage response = await ApiSykkel.Client.GetAsync($"https://gbfs.urbansharing.com/oslobysykkel.no/station_information.json"))
-            {
-                Station_Info data = await response.Content.ReadAsAsync<Station_Info>();
-
-                Console.WriteLine(data.Data.Stations[0].Address);
-              
-            }
-
-
-            using (HttpResponseMessage response2 = await ApiSykkel.Client.GetAsync($"https://gbfs.urbansharing.com/oslobysykkel.no/station_status.json"))
-            {
-                Station_Info data = await response2.Content.ReadAsAsync<Station_Info>();
-
-                Console.WriteLine(data.Data.Stations[0].Is_Returning);
-              
-            }
+            apiResponse();
 
 
 
         }
-        
 
+
+        private async void apiResponse()
+        {
+            try
+            {
+                using (HttpResponseMessage response = await ApiSykkel.Client.GetAsync($"https://gbfs.urbansharing.com/oslobysykkel.no/station_information.json"))
+                {
+
+
+                    using (HttpResponseMessage response2 = await ApiSykkel.Client.GetAsync($"https://gbfs.urbansharing.com/oslobysykkel.no/station_status.json"))
+                    {
+                        if (!response.IsSuccessStatusCode && !response2.IsSuccessStatusCode)
+                            Console.WriteLine("Response returned unsuccesfull");
+                        else
+                        {
+                            Station_Info station_status_data = await response2.Content.ReadAsAsync<Station_Info>();
+
+                            Console.WriteLine(station_status_data.Data.Stations[0].Is_Returning);
+
+                            Station_Info station_info_data = await response.Content.ReadAsAsync<Station_Info>();
+
+                            Console.WriteLine(station_info_data.Data.Stations[0].Address);
+                        }
+
+
+
+                    }
+
+                }
+            }catch(HttpRequestException e)
+            {
+                Console.WriteLine(e.Message);
+            }catch(ArgumentNullException a)
+            {
+                Console.WriteLine(a.Message);
+            }
+        }
 
     }
 }
