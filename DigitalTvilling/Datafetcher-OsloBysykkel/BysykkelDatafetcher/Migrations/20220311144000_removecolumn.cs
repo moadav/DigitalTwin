9d0fe84336902aeea63e6c598dfdb55bcfb0313e
Kaminari <jonasnbr@hiof.no>
2022-03-15 21:13:04 +0100

    added azure function to fetch and store data in db
    azure function to fetch oslo bysykkel station data and store it in the azure database

---
+++ DigitalTvilling/Datafetcher-OsloBysykkel/BysykkelDatafetcher/Migrations/20220311144000_removecolumn.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BysykkelDatafetcher.Migrations
+{
+    public partial class removecolumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "created_at",
+                table: "Stations");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "created_at",
+                table: "Stations",
+                type: "datetime2",
+                nullable: false,
+                computedColumnSql: "GETDATE()");
+        }
+    }
+}
