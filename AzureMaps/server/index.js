10842887bf50585cb9d6b87041db68853c2ef53e
unknown <trond.k.waalen@hiof.no>
2022-04-13 15:34:53 +0200

    Remove tested code from index.js

--- AzureMaps/server/index.js
+++ AzureMaps/server/index.js
@@ -4,10 +4,8 @@ const { toArray } = require("ix/asynciterable");
 
 const express = require('express');
 const cors = require('cors');
-const bodyParser = require('body-parser');
 const app = express();
 const PORT = 8080;
-const path = require('path');
 
 const dt_link = "https://dthiofadt.api.weu.digitaltwins.azure.net";
 const client = new DigitalTwinsClient(dt_link, new DefaultAzureCredential);
@@ -16,39 +14,12 @@ app.use( express.json() )
 app.use( cors() );
 
 
-app.get('/test2', (req, res) => {
-    res.json({test3: "test3"})
-})
-
-app.get('/test', (req, res) => {
-
-
-    res.status(200).json({
-        test:"test"
-    });
-})
-    
-
 app.get('/digitaltwin', async (req, res) => {
     
     let dt = await getDigitalTwin("SELECT * FROM digitaltwins WHERE $dtId = 'Nordstrand'");
 
-    res.status(200).send({
-        test: "test",
-        dt: dt
-    })
-    
-    /*
-    await getDigitalTwin("SELECT * FROM digitaltwins WHERE $dtId = 'Nordstrand'")
-        .then(r => {
-            return res.status(200).send({
-                test:"test2",
-                dt: r
-            })
-        });
-    */
+    res.status(200).send(dt.value[0])
 
-    //res.status(200).send({});
 });
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}.`));
