108fef612265c4f576fae0078cc3c813b7fe4dbc
unknown <trond.k.waalen@hiof.no>
2022-04-14 13:17:32 +0200

    Remove old code

--- AzureMaps/updateStations.js
+++ AzureMaps/updateStations.js
@@ -5,10 +5,6 @@ async function updateStations(bikeStations) {
             return response.json();
         })
         .then(data => {
-            console.log('-------------------------------------------')
-            console.log('data from /bikestations (direct from digital twin)...')
-            console.log(data)
-            console.log('-------------------------------------------')
             updateBikeStations(bikeStations, data);
         })
         .catch(error => {
@@ -16,9 +12,7 @@ async function updateStations(bikeStations) {
         });
 }
 
-
 function updateBikeStations(bikeStations, data) {
-
     // bikeStations = stations.js (geojson object)
     // data = data from digital twin (real time data)
 
@@ -29,21 +23,7 @@ function updateBikeStations(bikeStations, data) {
         bikeStationsMap.set(object.Station_information.Station_Id, object);
     });
 
-    // extract array from geojson
-    let bikeStationsArray = [...bikeStations.features];
-
-    //console.log(bikeStationsArray[0]);
-    //console.log(bikeStationsArray[0].properties)
-    //console.log(typeof bikeStationsArray[0].properties.station_id);
-    //console.log(typeof parseInt(bikeStationsArray[0].properties.station_id));
-
-    let testStationId = parseInt(bikeStationsArray[0].properties.station_id);
-    let testMap = bikeStationsMap.get(testStationId);
-    //console.log(testMap);
-    //console.log(testMap.Bicycle_Status.Station_Status.Bicycle_Available.Num_Bikes_Available);
-
     // update values in geojson array
-    //bikeStationsArray.forEach(station => {
     bikeStations.features.forEach(station => {
         let stationId = parseInt(station.properties.station_id);
         let stationDataObject = bikeStationsMap.get(stationId);
@@ -54,6 +34,5 @@ function updateBikeStations(bikeStations, data) {
         station.properties.available_bikes = availableBikes;
         station.properties.available_docks = availableDocks;
     })
-
 
 }
