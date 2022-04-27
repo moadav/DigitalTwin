d19c8c81b1d1a91f698fe32d08f2e0bceba13322
Kaminari <jonasnbr@hiof.no>
2022-04-27 23:30:12 +0200

    autoformat

--- AzureMaps/simulation.js
+++ AzureMaps/simulation.js
@@ -1,80 +1,73 @@
-
 async function testGetMLStudioResults() {
-    let stationName, day, time, temperature ;
+  let stationName, day, time, temperature
 
-    let resultsDiv = document.getElementById('simulation_result');
+  let resultsDiv = document.getElementById("simulation_result")
 
-    stationName = document.getElementById('myInput').value;
-    day = document.getElementById('select_day').value;
-    time = document.getElementById('select_hours').value;
-    temperature = document.getElementById('weather_temp').value;
+  stationName = document.getElementById("myInput").value
+  day = document.getElementById("select_day").value
+  time = document.getElementById("select_hours").value
+  temperature = document.getElementById("weather_temp").value
 
-    let stationId = getStationId(stationName);
-    let validated = formValidation(stationName, day, time, temperature);
-   
-    if(validated) {
+  let stationId = getStationId(stationName)
+  let validated = formValidation(stationName, day, time, temperature)
 
-        let body = {
-            stationId: stationId,
-            day: day,
-            time: time,
-            temperature: temperature
-        }
-
-        let options = {
-            method: 'POST',
-            body: JSON.stringify(body),
-            headers: {
-                'Content-Type': 'application/json'
-            },
-        }
-    
-        await fetch('http://localhost:8080/mlstudio', options)
-            .then(response => {
-                return response.json();
-            })
-            .then(data => {
-                let results = data.Results.WebServiceOutput0[0]['Scored Labels'];
-                resultsDiv.innerHTML = `Estimated number of available bikes: <br/> ${Math.round(results)}`;
-            })
-            .catch(error => {
-                console.log(error);
-            });
-    } else {
-        resultsDiv.innerHTML = '';
+  if (validated) {
+    let body = {
+      stationId: stationId,
+      day: day,
+      time: time,
+      temperature: temperature,
     }
 
+    let options = {
+      method: "POST",
+      body: JSON.stringify(body),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
 
-
+    await fetch("http://localhost:8080/mlstudio", options)
+      .then((response) => {
+        return response.json()
+      })
+      .then((data) => {
+        let results = data.Results.WebServiceOutput0[0]["Scored Labels"]
+        resultsDiv.innerHTML = `Estimated number of available bikes: <br/> ${Math.round(
+          results
+        )}`
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  } else {
+    resultsDiv.innerHTML = ""
+  }
 }
-
-
 
 function formValidation(stationName, day, time, temperature) {
-    let errorDiv = document.getElementById('form_error');
-    let errorMessage = ''
+  let errorDiv = document.getElementById("form_error")
+  let errorMessage = ""
 
-    if(stationName && day && time && temperature) {
-        errorDiv.innerHTML = errorMessage;
-        return true;
-    } 
+  if (stationName && day && time && temperature) {
+    errorDiv.innerHTML = errorMessage
+    return true
+  }
 
-    errorMessage = 'Please fill out all fields.' 
-    errorDiv.innerHTML = errorMessage;
-    return false;
+  errorMessage = "Please fill out all fields."
+  errorDiv.innerHTML = errorMessage
+  return false
 }
 
-
 function getStationId(stationName) {
-    let id;
+  let id
 
-    bikeStations.features.forEach(station => {
-        if(stationName === station.properties.name) {
-            id = station.properties.station_id;
-            return id;
-        }
-    });
+  bikeStations.features.forEach((station) => {
+    if (stationName === station.properties.name) {
+      id = station.properties.station_id
+      return id
+    }
+  })
 
-    return id;
-}   
-
+  return id
+}
